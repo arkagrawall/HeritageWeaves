be5ed4f8a12aa92913f7c8590f5170ad5a2aad08
unknownprogrammer12 <rajji6295@gmail.com>
2023-07-10 02:39:16 +0530

    haywire6.0

---
+++ admin/inc/config.php
@@ -0,0 +1,32 @@
+<?php
+// Error Reporting Turn On
+ini_set('error_reporting', E_ALL);
+
+// Setting up the time zone
+date_default_timezone_set('Asia/Kolkata');
+
+// Host Name
+$dbhost = 'sql306.infinityfree.com';
+
+// Database Name
+$dbname = 'if0_34521020_heritage';
+
+// Database Username
+$dbuser = 'if0_34521020';
+
+// Database Password
+$dbpass = 'FzyGOgNTbs8Mmb';
+
+// Defining base url
+define("BASE_URL", "");
+
+// Getting Admin url
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+try {
+	$pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+	echo "Connection error :" . $exception->getMessage();
+}
---
+++ test.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'noreply@bot.free.nf';
+$recipient = 'rainbowsalt62@bot.free.nf';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
